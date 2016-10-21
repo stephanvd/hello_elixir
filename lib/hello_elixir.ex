a9eb4ff2687bfafd2e1849675e4b57835ca16c80
Stephan van Diepen <svdiepen@zilverline.com>
2016-10-21 15:31:39 +0200

    Implement OrderPaid and OrderClosed events

--- lib/hello_elixir.ex
+++ lib/hello_elixir.ex
@@ -7,33 +7,55 @@ defmodule CQRS.AggregateRoot do
 end
 
 defmodule Order do
-  defstruct name: "", open: false, items: []
+  defstruct name: "", open: false, items: [], balance: 0.0, tip_jar: 0.0
 
   use CQRS.AggregateRoot
 
   # Exceptions
 
   defmodule OrderNotOpenException, do: defexception message: "Order is not open"
+  defmodule OrderHasBalanceException, do: defexception message: "Order has balance and can't be closed"
 
   # Values
 
-  defmodule Food, do: defstruct name: "", quantity: 1, price: 0.0
+  defmodule Item, do: defstruct name: "", quantity: 1, price: 0.0
 
   # Events
 
   defmodule OrderStarted, do: defstruct [:name]
   defmodule FoodOrdered, do: defstruct [:items]
+  defmodule OrderPaid, do: defstruct [:amount]
+  defmodule OrderClosed, do: defstruct []
 
   def apply(order, %OrderStarted{name: name}), do: %Order{order | name: name, open: true}
 
-  def apply(%Order{open: true} = order, %FoodOrdered{items: items}), do: %Order{order | items: items}
-  def apply(%Order{open: false}, %FoodOrdered{}), do: raise OrderNotOpenException
+  def apply(%Order{open: false}, _event), do: raise OrderNotOpenException
+
+  def apply(order, %FoodOrdered{items: items}), do: %Order{order | items: items, balance: sum_items(items)}
+
+  def apply(%Order{balance: balance} = order, %OrderPaid{amount: amount}) when amount <= balance do
+    %Order{order | balance: order.balance - amount}
+  end
+  def apply(%Order{balance: balance} = order, %OrderPaid{amount: amount}) when amount > balance do
+    %Order{order | balance: 0.0, tip_jar: amount - order.balance}
+  end
+
+  def apply(%Order{balance: 0.0} = order, %OrderClosed{}), do: %Order{order | open: false}
+  def apply(%Order{}, %OrderClosed{}), do: raise OrderHasBalanceException
 
   # Commands
 
   defmodule StartOrder, do: defstruct [:name]
   defmodule OrderFood, do: defstruct [:items]
+  defmodule PayOrder, do: defstruct [:amount]
+  defmodule CloseOrder, do: defstruct []
 
   def perform(%StartOrder{name: name}), do: [%OrderStarted{name: name}]
   def perform(%OrderFood{items: items}), do: [%FoodOrdered{items: items}]
+  def perform(%PayOrder{amount: amount}), do: [%OrderPaid{amount: amount}]
+  def perform(%CloseOrder{}), do: [%OrderClosed{}]
+
+  # Utils
+
+  defp sum_items(items), do: Enum.reduce(items, 0.0, fn(elem, acc) -> acc + (elem.price * elem.quantity) end)
 end
--- test/hello_elixir_test.exs
+++ test/hello_elixir_test.exs
@@ -3,11 +3,16 @@ defmodule OrderTest do
 
   alias Order.{
     OrderNotOpenException,
-    Food,
+    OrderHasBalanceException,
+    Item,
     OrderStarted,
     FoodOrdered,
+    OrderPaid,
+    OrderClosed,
     StartOrder,
     OrderFood,
+    PayOrder,
+    CloseOrder
   }
 
   # Commands
@@ -18,9 +23,19 @@ defmodule OrderTest do
   end
 
   test OrderFood do
-    items = [%Food{name: "Big Belly Burger", quantity: 1, price: 5.15}]
+    items = [%Item{name: "Big Belly Burger", quantity: 1, price: 5.15}]
     command = %OrderFood{items: items}
     assert Order.perform(command) == [%FoodOrdered{items: items}]
+  end
+
+  test CloseOrder do
+    command = %CloseOrder{}
+    assert Order.perform(command) == [%OrderClosed{}]
+  end
+
+  test PayOrder do
+    command = %PayOrder{amount: 5}
+    assert Order.perform(command) == [%OrderPaid{amount: 5}]
   end
 
   # Events
@@ -33,20 +48,69 @@ defmodule OrderTest do
   end
 
   test FoodOrdered do
-    items = [%Food{name: "Big Belly Burger", quantity: 1, price: 5.15}]
+    items = [%Item{name: "Big Belly Burger", quantity: 1, price: 5.15},
+             %Item{name: "Nuka Cola", quantity: 2, price: 4.20}]
     events = [%OrderStarted{name: "Zilverline"},
               %FoodOrdered{items: items}]
     order = %Order{} |> Order.replay(events)
 
-    assert order == %Order{name: "Zilverline", open: true, items: items}
+    assert order == %Order{name: "Zilverline", open: true, items: items, balance: 13.55}
   end
 
   test "FoodOrdered for an order that's not open" do
-    items = [%Food{name: "Big Belly Burger", quantity: 1, price: 5.15}]
+    items = [%Item{name: "Big Belly Burger", quantity: 1, price: 5.15}]
     events = [%FoodOrdered{items: items}]
 
     assert_raise OrderNotOpenException, fn ->
       Order.replay(%Order{}, events)
+    end
+  end
+
+  test OrderPaid do
+    items = [%Item{name: "Big Belly Burger", quantity: 1, price: 5.15}]
+    events = [%OrderStarted{name: "Zilverline"},
+              %FoodOrdered{items: items},
+              %OrderPaid{amount: 5.15}]
+    order = %Order{} |> Order.replay(events)
+
+    assert order == %Order{name: "Zilverline", open: true, items: items, balance: 0.0}
+  end
+
+  test "OrderPaid amount exceeding balance goes to the tip jar" do
+    items = [%Item{name: "Big Belly Burger", quantity: 1, price: 5.15}]
+    events = [%OrderStarted{name: "Zilverline"},
+              %FoodOrdered{items: items},
+              %OrderPaid{amount: 7.0}]
+    order = %Order{} |> Order.replay(events)
+
+    assert order == %Order{name: "Zilverline", open: true, items: items, balance: 0.0, tip_jar: 1.8499999999999996}
+  end
+
+  test OrderClosed do
+    events = [%OrderStarted{name: "Zilverline"},
+              %OrderClosed{}]
+    order = %Order{} |> Order.replay(events)
+
+    assert order == %Order{name: "Zilverline", open: false, items: []}
+  end
+
+  test "OrderClosed for an order that's already closed" do
+    events = [%OrderClosed{}]
+
+    assert_raise OrderNotOpenException, fn ->
+      %Order{} |> Order.replay(events)
+    end
+  end
+
+  test "OrderClosed for an order with outstanding balance" do
+    items = [%Item{name: "Big Belly Burger", quantity: 1, price: 5.15}]
+    events = [%OrderStarted{name: "Zilverline"},
+              %FoodOrdered{items: items},
+              %OrderPaid{amount: 2.0},
+              %OrderClosed{}]
+
+    assert_raise OrderHasBalanceException, fn ->
+      %Order{} |> Order.replay(events)
     end
   end
 end
